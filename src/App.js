fbbf26994eb8b6a6687d641a38f24e6c0def1309
bourb0n1412 <heikoho1@icloud.com>
2024-08-31 13:00:56 +0200

    Implementation of the homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
